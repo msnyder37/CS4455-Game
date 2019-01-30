98c889d640f3a8e5202bd5e6e7a5eb9934edd555
hson34 <edwardson16@gmail.com>
2019-01-30 02:21:25 -0500

    FieldCreated

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Obstacle"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}

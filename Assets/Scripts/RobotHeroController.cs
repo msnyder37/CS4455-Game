45711e5de55eb1ca5af47189b02c108534a67caa
Joey Sterling <jsterling30@gatech.edu>
2019-02-18 21:56:35 -0500

    begin work on robot animations with blend trees.

---
+++ Assets/Scripts/RobotHeroController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotHeroController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxisRaw("Vertical");
+        float turn = Input.GetAxisRaw("Horizontal");
+
+        this.animator.SetFloat("Forward", forward);
+        this.animator.SetFloat("Turn", turn);
+    }
+}

6bdc87ab932605b9808487511cf96050c979d4f1
bishoy33 <bishoymetry@hotmail.co.uk>
2019-03-03 00:22:27 -0500

    added features as outlined in the branch name

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+    public float timeLimit = 120;
+    public Text timeLimitDisplay;  
+    int min;
+    int sec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLimit -= Time.deltaTime;
+        min = Mathf.FloorToInt(timeLimit / 60);
+        sec = Mathf.FloorToInt(timeLimit % 60);
+        if ( timeLimit < 0 )
+        {
+           SceneManager.LoadScene ("GameOver");
+        }
+
+        timeLimitDisplay.text = min.ToString("00") + ":" + sec.ToString("00");
+    }
+
+}

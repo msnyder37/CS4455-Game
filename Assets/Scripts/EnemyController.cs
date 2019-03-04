558f4af609ed8842f6e43eff9c8ee86fd3dc9e45
gecage952 <gcage6@gatech.edu>
2019-03-03 22:38:44 -0500

    Added four basic enemies that patrol set areas and when they see the player they log a message

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    //credit to https://answers.unity.com/questions/989092/do-you-know-any-patrolling-ai-script-for-a-navmesh.html
+    public GameObject player;
+    public GameObject[] patrolPoints;
+    private int patrolPoint = 0;
+    public NavMeshAgent agent;
+    public float stoppingDistance;
+
+    void Start()
+    {
+        stoppingDistance = .4f;
+    }
+
+    void Update()
+    {
+        if(!Physics.Linecast(transform.position,player.transform.position,1)){ //check if we see player by linecasting,move player to another layer so the ray won't hit it.
+             Attack();
+        }else {
+             Patrol();
+        }
+    }
+
+    void Patrol(){
+        agent.Resume();
+        if (!agent.pathPending && agent.remainingDistance < stoppingDistance) {
+            if(patrolPoints.Length > 0){
+                agent.SetDestination(patrolPoints[patrolPoint].transform.position);
+
+                patrolPoint++;    //use distance if needed(lower precision)
+
+                if(patrolPoint >= patrolPoints.Length){
+                    patrolPoint = 0;
+                }
+            }
+        }
+    }
+
+    void Attack(){
+        agent.Stop();
+        Debug.Log("I see player");
+
+    }
+
+}

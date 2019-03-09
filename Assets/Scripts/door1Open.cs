e35f0f0c8ac30fbd0dc27d0f3362221b90f0a03f
gecage952 <gcage6@gatech.edu>
2019-03-09 02:47:12 -0500

    Added sound effects for player and enemy gunshots, and door opening, added a respawn location right before bridge section

--- Assets/Scripts/door1Open.cs
+++ Assets/Scripts/door1Open.cs
@@ -12,6 +12,7 @@ public class door1Open : MonoBehaviour
         if (other.gameObject.tag == "Player" && playerScript.hasKey1 == true)
         {
             animator.enabled = true;
+            GetComponent<AudioSource>().Play();
         }
     }
 }

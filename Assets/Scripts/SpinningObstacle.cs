e405ba28fc9b56f1c661d970a41eb5ef6420c69c
Joey Sterling <jsterling30@gatech.edu>
2019-03-09 22:17:04 -0500

    mouse controls for turning

--- Assets/Scripts/SpinningObstacle.cs
+++ Assets/Scripts/SpinningObstacle.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class SpinningObstacle : MonoBehaviour
 {
-    public float spin_speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        spin_speed = 2f;
-    }
+    public float spin_speed = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, spin_speed, 0);
-            //Rotaional speed of object
+        //Rotaional speed of object
     }
 }

a6497c7cdffe451c7b6ba2d19f1d2c202ecf6ab3
hson34 <edwardson16@gmail.com>
2019-04-07 21:25:56 -0400

    Level2Update4/7

---
+++ Assets/UnderGroundPassageScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderGroundPassageScript : MonoBehaviour
+{
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    float timer = 0.0f;
+    int second;
+    void Update()
+    {
+        timer += Time.deltaTime;
+        second = (int)(timer);
+        if (second > 10)
+        {
+            anim.enabled = false;
+        }
+        Debug.Log(timer);
+    }
+
+    bool stop = false;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("                                                " + other);
+        if (!stop)
+        {
+            if (other.gameObject.tag == "Destructable")
+            {
+                anim.enabled = false;
+            }
+            else
+            {
+                anim.enabled = true;
+                stop = true;
+            }
+        }
+        
+    }
+    
+}

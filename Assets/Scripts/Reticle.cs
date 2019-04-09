3f255285bd78e3047f773b0d50570e07574998b5
Joey Sterling <jsterling30@gatech.edu>
2019-04-09 19:30:05 -0400

    reticle fix in gameplay

---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false; // hide default mouse
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Input.mousePosition;
+    }
+}

888b4510030db3e47af4ebf5083263ff59962d5a
hson34 <edwardson16@gmail.com>
2019-04-22 20:12:33 -0400

    Override issue
    changed the name of each anim, and script of door3

---
+++ Assets/ButtonScript2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isPressed;
+    public Material[] materials;
+    public MeshRenderer rend;
+    public int index = 0;
+
+    void Start()
+    {
+        isPressed = false;
+        rend = GetComponent<MeshRenderer>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Sound Effect
+    }
+}

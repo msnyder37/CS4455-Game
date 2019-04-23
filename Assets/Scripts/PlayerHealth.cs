c3ac6c6e2de91709f68fbcecc6578e70dda0570d
bishoy33 <bishoymetry@hotmail.co.uk>
2019-04-23 17:46:27 -0400

    ammo counter removed, key3 scaled, fading transition start

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float health_player = 1.00f;    //health is ranges between 0.00 to 1.00
-    public Slider healthbar;  
+    public float health_player = 0.30f; //1.00f;    //health is ranges between 0.00 to 1.00
+    public Slider healthbar;
+    public Animator animator;  
+    private bool animation_started = false;
 
     
     void Update()
@@ -17,7 +19,28 @@ public class PlayerHealth : MonoBehaviour
 
         //If healthbar drops too low, load the game over screen
         if (health_player < 0.01f){
+            animator.SetTrigger("fadeout");     //fades out scene
             SceneManager.LoadScene ("GameOver");
+            
+           // bool AnimatorIsPlaying(){
+           // return animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+          //  }
+
+
+/*
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("fade_out"))
+            {
+                Debug.Log("Hi");
+                animation_started = true;
+            }
+            else{
+                // SceneManager.LoadScene ("GameOver");
+                if (animation_started = true){
+                     Debug.Log("Hello");
+                }
+               
+            }
+            */
         }
     }
 }
